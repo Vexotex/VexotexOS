78ea2c91d3646de08ace3a5a461f981135852dc4
Vexotex <156705937+Vexotex@users.noreply.github.com>
2026-05-06 21:43:39 +0200

    fuzzyfinder

--- hosts/Laptop/LaptopConfiguration.nix
+++ hosts/Laptop/LaptopConfiguration.nix
@@ -63,6 +63,7 @@
             neovim
             unzip
             fd # Fast file finder.
+            fzf # Command-line fuzzy finder.
             ripgrep # Fast search tool, alternative to grep.
             htop # Interactive process viewer.
             gotop
