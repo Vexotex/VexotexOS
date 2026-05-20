8f53a2f23aee360018a2bbaa2f79aeb021023422
Vexotex <156705937+Vexotex@users.noreply.github.com>
2026-05-20 15:04:29 +0200

    dupelicate entfernt

--- modules/system/docker.nix
+++ modules/system/docker.nix
@@ -2,7 +2,7 @@
 
     flake.systemModules.Docker = { ... }: {
 
-        virtualisation.docker.enable = true;virtualisation.docker.enable = true;
+        virtualisation.docker.enable = true;
         users.extraGroups.docker.members = [ "Vexotex" ];
 
     };
